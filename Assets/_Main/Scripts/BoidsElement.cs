07ebfd648db7c5e95d25250a9e098ce665414f3d
mikinel <mikki.stad@gmail.com>
2021-12-05 04:39:23 +0900

    つくったけどうまくいかん

---
+++ Assets/_Main/Scripts/BoidsElement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UdonSharp;
+using UnityEngine;
+
+public class BoidsElement : UdonSharpBehaviour
+{
+    public Vector3 Vector => vector;
+    [SerializeField] private Vector3 vector;
+    [SerializeField] private float speed = 0.01f;
+
+    [Space]
+    [SerializeField] public BoidsManager BoidsManager;
+
+    private void Update()
+    {
+        if (BoidsManager == null)
+            return;
+
+        var nearElements = BoidsManager.GetNearObjects(this.transform.position);
+
+        Vector3 separationVector = Vector3.zero;
+        Vector3 vecBuff = Vector3.zero;
+        Vector3 pos = Vector3.zero;
+
+        for (int i = 0; i < nearElements.Length; i++)
+        {
+            if (nearElements[i] == null)
+                break;
+
+            //分離
+            var dif = this.transform.position - nearElements[i].transform.position;
+            separationVector += dif;
+
+            //整列
+            vecBuff += nearElements[i].Vector;
+
+            //結合
+            pos += nearElements[i].transform.position;
+        }
+
+        vector += separationVector / nearElements.Length;
+
+        vector += (vecBuff / nearElements.Length) - Vector;
+
+        vector += (pos / nearElements.Length) - this.transform.position;
+
+        var newPos = transform.position + vector;
+        Debug.Log(newPos);
+
+        if (newPos.x != float.NaN && newPos.y != float.NaN && newPos.z != float.NaN)
+            transform.position += vector;
+    }
+
+    //private void LateUpdate()
+    //{
+    //    var newPos = transform.position + vector;
+    //    Debug.Log(newPos);
+
+    //    if(newPos.x != float.NaN && newPos.y != float.NaN && newPos.z != float.NaN)
+    //        transform.position += vector;
+    //}
+}
---
+++ Assets/_Main/Scripts/BoidsManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UdonSharp;
+using UnityEngine;
+
+public class BoidsManager : UdonSharpBehaviour
+{
+    [SerializeField] private Transform boidsElementParent;
+    [SerializeField] private BoidsElement[] boidsElements;
+
+    //SETTINGS
+    [Space]
+    [SerializeField] private float distanceThreashould;
+    [SerializeField] private int boidsElementsMaxNum = 512;
+
+    private void Start()
+    {
+        boidsElements = new BoidsElement[boidsElementParent.childCount];
+
+        for (int i = 0; i < boidsElementParent.childCount; i++)
+        {
+            if (boidsElementsMaxNum < i)
+                return;
+
+            var g = boidsElementParent.GetChild(i).gameObject;
+            var boidsElement = g.GetComponent<BoidsElement>();
+            boidsElement.BoidsManager = this;
+            boidsElements[i] = boidsElement;
+        }
+    }
+
+    /// <summary>
+    /// 与えた距離から近いオブジェクトを取得する
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <returns></returns>
+    public BoidsElement[] GetNearObjects(Vector3 pos)
+    {
+        BoidsElement[] res = new BoidsElement[boidsElementsMaxNum];
+        int index = 0;
+
+        foreach (var boidsElement in boidsElements)
+        {
+            if(Vector3.Distance(boidsElement.transform.position, pos) < distanceThreashould)
+            {
+                res[index] = boidsElement;
+                index++;
+            }
+        }
+
+        return res;
+    }
+}
