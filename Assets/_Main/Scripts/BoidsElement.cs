147525604708a0f2bff9a7245b3cd401b49380d2
mikinel <mikki.stad@gmail.com>
2021-12-05 13:09:08 +0900

    upd

--- Assets/_Main/Scripts/BoidsElement.cs
+++ Assets/_Main/Scripts/BoidsElement.cs
@@ -1,63 +1,78 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 
 public class BoidsElement : UdonSharpBehaviour
 {
-    public Vector3 Vector => vector;
-    [SerializeField] private Vector3 vector;
-    [SerializeField] private float speed = 0.01f;
+    // Reference to the controller.
+    public BoidsManager BoidsManager;
 
-    [Space]
-    [SerializeField] public BoidsManager BoidsManager;
+    // Options for animation playback.
+    public float animationSpeedVariation = 0.2f;
 
-    private void Update()
+    // Random seed.
+    float noiseOffset;
+
+    // Caluculates the separation vector with a target.
+    Vector3 GetSeparationVector(Transform target)
+    {
+        var diff = transform.position - target.transform.position;
+        var diffLen = diff.magnitude;
+        var scaler = Mathf.Clamp01(1.0f - diffLen / BoidsManager.neighborDist);
+        return diff * (scaler / diffLen);
+    }
+
+    void Start()
+    {
+        noiseOffset = Random.value * 10.0f;
+    }
+
+    void Update()
     {
         if (BoidsManager == null)
             return;
 
-        var nearElements = BoidsManager.GetNearObjects(this.transform.position);
+        var currentPosition = transform.position;
+        var currentRotation = transform.rotation;
 
-        Vector3 separationVector = Vector3.zero;
-        Vector3 vecBuff = Vector3.zero;
-        Vector3 pos = Vector3.zero;
+        // Current velocity randomized with noise.
+        var noise = Mathf.PerlinNoise(Time.time, noiseOffset) * 2.0f - 1.0f;
+        var velocity = BoidsManager.velocity * (1.0f + noise * BoidsManager.velocityVariation);
 
-        for (int i = 0; i < nearElements.Length; i++)
+        // Initializes the vectors.
+        var separation = Vector3.zero;
+        var alignment = BoidsManager.transform.forward;
+        var cohesion = BoidsManager.transform.position;
+
+        // Looks up nearby boids.
+        var nearbyBoids = Physics.OverlapSphere(currentPosition, BoidsManager.neighborDist, BoidsManager.SearchLayer);
+
+        // Accumulates the vectors.
+        foreach (var boid in nearbyBoids)
         {
-            if (nearElements[i] == null)
-                break;
-
-            //分離
-            var dif = this.transform.position - nearElements[i].transform.position;
-            separationVector += dif;
-
-            //整列
-            vecBuff += nearElements[i].Vector;
-
-            //結合
-            pos += nearElements[i].transform.position;
+            if (boid.gameObject == gameObject) continue;
+            var t = boid.transform;
+            separation += GetSeparationVector(t);
+            alignment += t.forward;
+            cohesion += t.position;
         }
 
-        vector += separationVector / nearElements.Length;
+        var avg = 1.0f / nearbyBoids.Length;
+        alignment *= avg;
+        cohesion *= avg;
+        cohesion = (cohesion - currentPosition).normalized;
 
-        vector += (vecBuff / nearElements.Length) - Vector;
+        // Calculates a rotation from the vectors.
+        var direction = separation + alignment + cohesion;
+        var rotation = Quaternion.FromToRotation(Vector3.forward, direction.normalized);
 
-        vector += (pos / nearElements.Length) - this.transform.position;
+        // Applys the rotation with interpolation.
+        if (rotation != currentRotation)
+        {
+            var ip = Mathf.Exp(-BoidsManager.rotationCoeff * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(rotation, currentRotation, ip);
+        }
 
-        var newPos = transform.position + vector;
-        Debug.Log(newPos);
-
-        if (newPos.x != float.NaN && newPos.y != float.NaN && newPos.z != float.NaN)
-            transform.position += vector;
+        // Moves forawrd.
+        transform.position = currentPosition + transform.forward * (velocity * Time.deltaTime);
     }
-
-    //private void LateUpdate()
-    //{
-    //    var newPos = transform.position + vector;
-    //    Debug.Log(newPos);
-
-    //    if(newPos.x != float.NaN && newPos.y != float.NaN && newPos.z != float.NaN)
-    //        transform.position += vector;
-    //}
 }
--- Assets/_Main/Scripts/BoidsManager.cs
+++ Assets/_Main/Scripts/BoidsManager.cs
@@ -1,53 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 
 public class BoidsManager : UdonSharpBehaviour
 {
     [SerializeField] private Transform boidsElementParent;
-    [SerializeField] private BoidsElement[] boidsElements;
+    [SerializeField] private GameObject[] boidsElements;
 
-    //SETTINGS
-    [Space]
-    [SerializeField] private float distanceThreashould;
-    [SerializeField] private int boidsElementsMaxNum = 512;
+    [Range(0.1f, 20.0f)]
+    public float velocity = 6.0f;
+
+    [Range(0.0f, 0.9f)]
+    public float velocityVariation = 0.5f;
+
+    [Range(0.1f, 20.0f)]
+    public float rotationCoeff = 4.0f;
+
+    [Range(0.1f, 10.0f)]
+    public float neighborDist = 2.0f;
+
+    public LayerMask SearchLayer;
 
     private void Start()
     {
-        boidsElements = new BoidsElement[boidsElementParent.childCount];
+        boidsElements = new GameObject[boidsElementParent.childCount];
 
         for (int i = 0; i < boidsElementParent.childCount; i++)
         {
-            if (boidsElementsMaxNum < i)
-                return;
+            boidsElements[i] = boidsElementParent.GetChild(i).gameObject;
 
-            var g = boidsElementParent.GetChild(i).gameObject;
-            var boidsElement = g.GetComponent<BoidsElement>();
-            boidsElement.BoidsManager = this;
-            boidsElements[i] = boidsElement;
+            var rotation = Quaternion.Slerp(transform.rotation, Random.rotation, 0.3f);
+            boidsElements[i].transform.rotation = rotation;
+            boidsElements[i].GetComponent<BoidsElement>().BoidsManager = this;
         }
-    }
-
-    /// <summary>
-    /// 与えた距離から近いオブジェクトを取得する
-    /// </summary>
-    /// <param name="pos"></param>
-    /// <returns></returns>
-    public BoidsElement[] GetNearObjects(Vector3 pos)
-    {
-        BoidsElement[] res = new BoidsElement[boidsElementsMaxNum];
-        int index = 0;
-
-        foreach (var boidsElement in boidsElements)
-        {
-            if(Vector3.Distance(boidsElement.transform.position, pos) < distanceThreashould)
-            {
-                res[index] = boidsElement;
-                index++;
-            }
-        }
-
-        return res;
     }
 }
